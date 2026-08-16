650504414d75b8f1c6dc1384fdcca3733ff9c00c
Fazmis <vlad.par.2004@gmail.com>
2026-08-16 17:10:44 +0300

    feat: add camera-based automatic brightness

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-import time
+import numpy as np
+import cv2
 from native import set_brightness
 
 
 def main() -> None:
-    set_brightness(100)
-    time.sleep(2)
-    set_brightness(0)
-    time.sleep(2)
-    set_brightness(50)
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print("Не удалось захватить камеру")
+        return
+
+    while True:
+        ret, frame = cap.read()
+        cv2.imshow("Photo", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+        frame_intensity_percent = round(frame.mean() / 255 * 100)
+        print(frame_intensity_percent)
+        brightness_level = frame_intensity_percent
+        set_brightness(brightness_level)
+    cap.release()
+    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
