81407e2b56daa55bc057942cfe613dd600af765e
Fazmis <vlad.par.2004@gmail.com>
2026-08-16 19:33:38 +0300

    feat: open camera only when measuring brightness

--- camera.py
+++ camera.py
@@ -1,3 +1,5 @@
+import time
+
 import cv2
 import numpy as np
 from typing import Any
@@ -6,9 +8,6 @@ from typing import Any
 class Camera:
     def __init__(self, camera_index=0):
         self.camera_index = camera_index
-        self.video_capture = cv2.VideoCapture(self.camera_index)
-        if not self.video_capture.isOpened():
-            raise RuntimeError("Ошибка захвата камеры")
 
     def _get_avg_color_intensity_percent(self) -> int:
         frame = self.get_frame()
@@ -16,14 +15,18 @@ class Camera:
         return frame_intensity_percent
 
     def get_frame(self) -> np.ndarray[Any, np.dtype]:
-        bool_status, frame = self.video_capture.read()
-        if not bool_status:
-            raise RuntimeError("Ошибка захвата кадра")
-        return frame
+        video_capture = cv2.VideoCapture(self.camera_index)
+        if not video_capture.isOpened():
+            raise RuntimeError("Ошибка захвата камеры")
+        try:
+            time.sleep(0.25)
+            bool_status, frame = video_capture.read()
+            if not bool_status:
+                raise RuntimeError("Ошибка захвата кадра")
+            return frame
+        finally:
+            video_capture.release()
 
     def get_brightness(self) -> int:
         frame_intensity_percent = self._get_avg_color_intensity_percent()
         return frame_intensity_percent
-
-    def release(self) -> None:
-        self.video_capture.release()
--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from camera import Camera
 
 
 def main() -> None:
+    set_brightness(50)
     camera = Camera()
-    try:
-        while True:
-            brightness_level = camera.get_brightness()
-            set_brightness(brightness_level)
-            time.sleep(3)
-    finally:
-        camera.release()
+    while True:
+        t0 = time.time()
+        brightness_level = camera.get_brightness()
+        t0 = time.time()
+        set_brightness(brightness_level)
+        time.sleep(3)
 
 
 if __name__ == '__main__':
